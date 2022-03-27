91b732aa658f8ffce2ac2939d90111e1d57e637a
alex-roh <roheunyoung@outlook.com>
2022-03-27 18:44:23 +0900

    Init blog project

---
+++ blog-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: "alex-roh.github.io",
+  description: "Just another ordinary payroller.",
+  author: "Alex",
+  siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
+  links: {
+    github: "https://github.com/alex-roh",
+    linkedIn: "https://linkedin.com",
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com",
+    email: "mailto:roheunyoung0@gmail.com",
+  },
+  utterances: {
+    repo: "alex-roh/alex-roh.github.io",
+    type: "pathname",
+  },
+}
